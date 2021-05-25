bf53b98c60789bbe66606889d7fc8bb2d2932b57
bnulens <brechtnulens@gmail.com>
2021-05-25 23:27:24 +0200

    Draw new svg into canvas

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,31 +1,32 @@
 <script>
-
 </script>
 
 <div class="content-wrapper">
-    <div class="content-inner-wrapper">
-        <span>XYZ</span>
-    </div>
+  <div class="content-inner-wrapper">
+    <span>XYZ</span>
+  </div>
+  <svg id="circSvg" width="10" height="10">
+    <circle cx="5" cy="5" r="4" stroke="black" stroke-width="2" fill="white" />
+  </svg>
 </div>
 
 <style lang="scss">
-    .content-wrapper {
-      display: flex;
-      left: 50%;
-      right: 50%;
-      width: 100vw;
-      max-width: 100vw;
-      margin-right: -50vw;
-      border: 1px solid blue;
+  .content-wrapper {
+    display: flex;
+    left: 50%;
+    right: 50%;
+    max-width: 95%;
+    margin-right: -50vw;
+    border: 1px solid blue;
 
-      .content-inner-wrapper {
-        max-width: 95%;
-        margin: 24px auto;
-        border: 1px solid green;
-      }
+    .content-inner-wrapper {
+      max-width: 95%;
+      margin: 24px auto;
+      border: 1px solid green;
     }
+  }
 
-    @media screen and (min-width: 676px) {
+  @media screen and (min-width: 676px) {
     .content-wrapper {
       .content-inner-wrapper {
         width: 95%;
--- src/Map.svelte
+++ src/Map.svelte
@@ -1,41 +1,41 @@
 <script>
-    import { onMount } from "svelte"
-    let canvas;
+  import { onMount } from "svelte";
+  let canvas;
 
-    onMount(() => {
-        let ctx = canvas.getContext("2d");
-        function draw() {
-            ctx.beginPath();
-            ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-            ctx.stroke(); 
-        }
-        draw();
-    })
+  onMount(() => {
+    let ctx = canvas.getContext("2d");
+    function draw() {
+      ctx.beginPath();
+      ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+    draw();
+  });
 </script>
 
 <div class="map-wrapper">
-    <canvas bind:this={canvas}/>
+  <canvas bind:this={canvas} />
 </div>
 
 <style lang="scss">
+  .map-wrapper {
+    display: flex;
+    border: 1px solid red;
+
+    canvas {
+      max-width: 95%;
+      width: 100vw;
+      height: 80vh;
+      margin: 0 auto;
+      border: 1px dotted black;
+    }
+  }
+
+  @media screen and (min-width: 676px) {
     .map-wrapper {
-        display: flex;
-        border: 1px solid red;
-
-        canvas {
-            max-width: 95%;
-            width: 100vw;
-            height: 80vh;
-            margin: 0 auto;
-            border: 1px dotted black;
-        }
+      canvas {
+        max-width: 1280px;
+      }
     }
-
-    @media screen and (min-width: 676px) {
-        .map-wrapper {
-            canvas {
-                max-width: 1280px;
-            }
-        }
-    }
+  }
 </style>
