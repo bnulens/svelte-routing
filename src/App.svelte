0a9046fc475fd3f014f9b813f995a42ed673d5ca
Brecht Nulens <brecht.nulens@netropolix.be>
2021-05-25 15:52:14 +0200

    Test drawings on canvas

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
 	import Navigation from "./Nav/Navigation.svelte"
 	import Content from "./Content.svelte"
+	import Map from "./Map.svelte"
 	
 </script>
 
 <main>
 	<Navigation/>
 	<Content/>
+	<Map/>
 </main>
 
 <style>
